657ae1a846ef3f3ee76fb408cf5b44321a61f4b2
Nguyen The Phong <phongghast@gmail.com>
2025-09-11 23:38:28 +0700

    rename

--- src/main/java/com/web/bookingKol/BookingKolApplication.java
+++ src/main/java/com/web/bookingKol/BookingKolApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookKolApplication {
+public class BookingKolApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookKolApplication.class, args);
+		SpringApplication.run(BookingKolApplication.class, args);
 	}
 
 }
