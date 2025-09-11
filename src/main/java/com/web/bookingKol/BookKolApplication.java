4e55bad72b389352095327f0da10c17364164d89
Nguyen The Phong <phongghast@gmail.com>
2025-09-11 23:31:02 +0700

    initial codebase

---
+++ src/main/java/com/web/bookingKol/BookKolApplication.java
@@ -0,0 +1,13 @@
+package com.web.bookingKol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookKolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookKolApplication.class, args);
+	}
+
+}
