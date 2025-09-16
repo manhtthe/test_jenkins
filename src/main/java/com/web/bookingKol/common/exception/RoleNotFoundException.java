d15c4167ebf962ba505a8c250c8cfb5f28434b3d
Nguyen The Phong <phongghast@gmail.com>
2025-09-16 16:54:07 +0700

    Revert "feat: create models, config spring security, login" My bads This reverts commit 04996df792ba74bef6f8479f634e009871460d67.

--- src/main/java/com/web/bookingKol/common/exception/RoleNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.web.bookingKol.common.exception;
-
-public class RoleNotFoundException extends Exception {
-    public RoleNotFoundException(String message) {
-        super(message);
-    }
-}
